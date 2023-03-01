21974020523bcbbc60333aa149290143c2c8bb7b
Ahmad Ghazzi <aghazzi.1998@gmail.com>
2023-03-01 15:58:54 +0200

    transaction, recurring

---
+++ advancedproject/database/migrations/2023_03_01_131020_create_transactions_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            
+            $table->string('title')->nullable();
+            $table->text('description')->nullable();
+            $table->integer('amount')->nullable();
+            $table->char('currency')->nullable();
+            
+            $table->integer('category_id')->unsigned()->nullable();
+            $table->foreign('category_id')->references('id')->on('categories');
+            
+            $table->integer('recurring_id')->unsigned()->nullable();
+            $table->foreign('recurring_id')->references('id')->on('recurrings')->onDelete('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transactions');
+    }
+};
