ceb6f4274254911b404f2c9dc4d48010cdada2a4
fatinaelomar3 <fatinaelomar3@gmail.com>
2023-03-02 15:16:59 +0200

    finish authentication

--- advancedproject/routes/api.php
+++ advancedproject/routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ProfitController;
+use App\Http\Controllers\AuthController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -32,3 +33,21 @@ Route::Get('/profit/{id}', [ProfitController::class, 'getProfit']);
 Route::Get('/profit', [ProfitController::class, 'getAllProfit']);
 Route::Patch('/profit/{id}', [ProfitController::class, 'editProfit']);
 Route::delete('/profit/{id}', [ProfitController::class, 'deleteProfit']);
+
+//Authentication routes
+
+Route::group([
+    'middleware' => 'api',
+    'prefix' => 'auth'
+], function ($router) {
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/register', [AuthController::class, 'register']);
+
+});
+
+Route::Patch('/updateuser/{id}', [AuthController::class, 'edituser']);
+Route::Get('/getuser/{id}', [AuthController::class, 'getadmin']);
+Route::Get('/getall', [AuthController::class, 'getAlluser']);
+Route::delete('/delete/{id}', [AuthController::class, 'deleteuser']);
+Route::post('/logout', [AuthController::class, 'logout']);
+Route::get('/user-profile', [AuthController::class, 'userProfile']);
